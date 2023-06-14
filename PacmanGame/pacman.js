a5043bd0724bb1a493fae6aef5f56c8def051934
AndyLiu314 <andyliu4002@gmail.com>
2023-06-13 19:33:32 -0700

    moved files into pacmangame folder

--- PacmanGame/pacman.js
+++ PacmanGame/pacman.js
@@ -3,7 +3,7 @@
 // Global variables
 var canvas;
 var gl;
-
+var debug
 var program
 
 // Getting the keyboard input
@@ -16,9 +16,9 @@ function getKey(key) {
 
 // pacman movement frames
 var pacman_up = [
-	vec2( -0.05, -0.05 ),
-	vec2(  0.0,  0.05 ),
-	vec2(  0.05, -0.05 )
+	vec2(  -0.85,  0.90 ),
+	vec2( -0.80, 0.80 ),
+	vec2(  -0.90, 0.80 )
 ];
 
 var pacman_left = [
@@ -39,18 +39,38 @@ var pacman_down = [
 	vec2(  0.0, -0.05 )    
 ];
 
-var vertexbuffer = [
-	vec2( -0.95, 0.95),
-	vec2(  0.95, 0.95),
-	vec2(  0.95, -0.95 ),
-	vec2( -0.95, 0.95),
-	vec2(  0.95, -0.95 ),
-	vec2( -0.95, -0.95 )
+var pathBuffer = [
+	vec2( -0.90, 0.90),
+	vec2(  0.90, 0.90),
+	vec2(  0.90, -0.90),
+	vec2( -0.90, 0.90),
+	vec2(  0.90, -0.90),
+	vec2(  -0.90, -0.90),
 ]; 
 
-var vertexbufferColor = vec4(0.7, 0.7, 0.7, 1.0);
+const wall = [
+	vec2( -0.90, 0.90),
+	vec2(  -0.70, 0.90),
+	vec2(  -0.70, 0.70),
+	vec2( -0.90, 0.90),
+	vec2(  -0.70, 0.70),
+	vec2(  -0.90, 0.70),
+];
 
-var pacmanColor = vec4(1.0, 0.0, 0.0, 1.0);
+var wall2 = [
+	[-0.90, 0.90],
+	[-0.70, 0.90],
+	[-0.70, 0.70],
+	[-0.90, 0.90],
+	[-0.70, 0.70],
+	[-0.90, 0.70]
+];
+
+var pathbufferColor = vec4(0.8, 0.8, 0.8, 1.0);
+
+var pacmanColor = vec4(0.0, 0.0, 1.0, 1.0);
+
+var wallColor = vec4(0.0, 0.6, 0.0, 1.0);
 
 // 0 = wall
 // 1 = path
@@ -100,6 +120,22 @@ function drawShape(type, vertices, n, color) {
 	gl.drawArrays(type, 0, n);
 }
 
+function translateObject(vertices, row, column, amountX, amountY) {
+	var newVertices = [];
+	//newVertices = vertices;
+	for (let i = 0; i < vertices.length; i++) {
+		newVertices.push(vec2(vertices[i][0], vertices[i][1]));
+	}
+
+	for (let i = 0; i < newVertices.length; i++){
+		let vertex = newVertices[i];
+		newVertices[i] = vec2(vertex[0] + row*amountX, vertex[1] - column*amountY);
+		//debug = newVertices[i][0];
+	}
+
+	return newVertices;
+}
+
 window.onload = function init() {
     canvas = document.getElementById( "gl-canvas" );
     gl = WebGLUtils.setupWebGL( canvas );
@@ -111,6 +147,17 @@ window.onload = function init() {
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
 	gl.useProgram( program );
 
+
+	newWall = translateObject(wall,1,0,0.2,0.2)
+	newWall2 = translateObject(wall,2,0,0.2,0.2)
+	newWall3 = translateObject(wall,3,0,0.2,0.2)
+	newWall4 = translateObject(wall,4,0,0.2,0.2)
+	newWall5 = translateObject(wall,5,0,0.2,0.2)
+	newWall6 = translateObject(wall,6,0,0.2,0.2)
+	newWall7 = translateObject(wall,7,0,0.2,0.2)
+	newWall8 = translateObject(wall,8,0,0.2,0.2)
+	newWall9 = translateObject(wall,9,0,0.2,0.2)
+	//newWall2 = translateObject(wall,3,0,0.2,0.2)
 	render();
 }
 
@@ -119,20 +166,32 @@ function render() {
 	
 	// For debugging 
 	//console.log(height);
-	//document.getElementById("debug").innerHTML = height;
+	document.getElementById("debug").innerHTML = debug;
 
-	drawShape(gl.TRIANGLES, vertexbuffer, 6, vertexbufferColor);
+	drawShape(gl.TRIANGLES, pathBuffer, 6, pathbufferColor);
 	drawShape(gl.TRIANGLES, pacman_up, 3, pacmanColor);
 
+	drawShape(gl.TRIANGLES, newWall, 6, wallColor);
+	drawShape(gl.TRIANGLES, newWall2, 6, wallColor);
+	/*
+	drawShape(gl.TRIANGLES, newWall3, 6, wallColor);
+	drawShape(gl.TRIANGLES, newWall4, 6, wallColor);
+	drawShape(gl.TRIANGLES, newWall5, 6, wallColor);
+	drawShape(gl.TRIANGLES, newWall6, 6, wallColor);
+	drawShape(gl.TRIANGLES, newWall7, 6, wallColor);
+	drawShape(gl.TRIANGLES, newWall8, 6, wallColor);*/ 
+	drawShape(gl.TRIANGLES, wall, 6, wallColor);
 	
+	/*
 	for (let row = 0; row < tilemap.length; row++){
 		for (let column = 0; column < tilemap[0].length; column++){
 			let grid = tilemap[row][column];
 			if (grid == 0){
-
+				//let translateWall = translateObject(wall, row, column, 0.2, 0.2);
+				//drawShape(gl.TRIANGLES, translateWall, 6, wallColor);
 			}
 		}
-	}
+	}*/
 	
 	window.requestAnimFrame(render);
 }
